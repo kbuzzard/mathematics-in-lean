36854642231235f3b38210cb0db21439b58f4b1e
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2019-01-01 22:59:59 +0000

    adding ch2 questions

---
+++ src/Exercises/02_and_or_forall_exists/ch02all.lean
@@ -0,0 +1,79 @@
+variables p q r : Prop
+
+example : p ∨ q → q ∨ p :=
+begin
+  sorry
+end
+
+example : p ∨ (q ∨ r) → (p ∨ q) ∨ r :=
+begin
+  sorry
+end
+
+example : p ∧ q → q ∧ p :=
+begin
+  sorry
+end
+
+example : p ∧ (q ∧ r) → (p ∧ q) ∧ r :=
+begin
+  sorry
+end
+
+example : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) :=
+begin
+  sorry
+end
+
+example : ¬ (p ∧ q) ↔ ¬ p ∨ ¬ q :=
+begin
+  sorry
+end
+
+example : ¬ (p ∨ q) ↔ ¬ p ∧ ¬ q :=
+begin
+  sorry
+end
+
+example : ¬ (p ↔ ¬ p) :=
+begin
+  sorry
+end
+
+example : p ∨ ¬ p :=
+begin
+  sorry
+end
+
+example : (p → q) ↔ (¬ q → ¬ p) :=
+begin
+  sorry
+end
+
+variables (X : Type) (P Q : X → Prop)
+
+example : ∀ x, P x ∧ Q x → ∀ x, Q x ∧ P x :=
+begin
+  sorry
+end
+
+example : ∃ x, P x ∨ Q x → ∃ x, Q x ∨ P x :=
+begin
+  sorry
+end
+
+example : (∀ x, P x) ∧ (∀ x, Q x) ↔ (∀ x, P x ∧ Q x) :=
+begin
+  sorry
+end
+
+example : (∃ x, P x) ∨ (∃ x, Q x) ↔ (∃ x, P x ∨ Q x) :=
+begin
+  sorry
+end
+
+example : ¬ (∀ x, P x) ↔ ∃ x, ¬ P x :=
+begin
+  sorry
+end
+
